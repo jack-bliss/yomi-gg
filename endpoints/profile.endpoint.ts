00d7dec44ba92c68ea757f383913dbbdc3b09b7c
Jack Bliss <jackbliss99@gmail.com>
2018-06-25 23:09:22 +0100

    bug fixing, second attempt at a server

--- endpoints/profile.endpoint.ts
+++ endpoints/profile.endpoint.ts
@@ -1,24 +1,23 @@
-import { Path, GET, PathParam, ContextRequest } from 'typescript-rest';
+import { Path, GET, ContextRequest } from 'typescript-rest';
 import { RequestExtended } from '../interfaces/request-extended.interface';
-import { Profile } from '../models/profile.model';
 
 @Path('/profile')
 export class ProfileEndpoint {
 
-  @Path(':id')
   @GET
   getProfileById(
-    @PathParam('id') id: number,
     @ContextRequest { pool }: RequestExtended,
   ): Promise<any> {
 
     const query = 'SELECT id,username,joined,type FROM profiles';
 
-    return new Promise((resolve, reject) => {
-      pool.query(query, [1], (err, result) => {
-        resolve(result);
-      });
-    });
+    return Promise.resolve(query);
+
+    // return new Promise((resolve, reject) => {
+    //   pool.query(query, [1], (err, result) => {
+    //     resolve(result);
+    //   });
+    // });
 
   }
 
--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
 import { Pool } from 'pg';
 import * as session from 'express-session';
-import * as PgSession from 'connect-pg-simple';
+import * as pgSession from 'connect-pg-simple';
 import { Server } from 'typescript-rest';
 
 import * as bcrypt from 'bcrypt';
 import { ProfileEndpoint } from './endpoints/profile.endpoint';
+import { RequestExtended } from './interfaces/request-extended.interface';
 
 
 const app: express.Application = express();
@@ -14,17 +15,19 @@ const port: number = parseInt(process.env.PORT) || 3000;
 
 const pool = new Pool();
 
+const PgSession = pgSession(session);
+
 app.use(session({
   store: new PgSession({
     pool,
-    tableName: 'session',
   }),
   secret: process.env.SECRET,
   resave: false,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  saveUninitialized: false,
 }));
 
-app.use((req, res, next) => {
+app.use((req: RequestExtended, res, next) => {
   req.pool = pool;
   next();
 });
