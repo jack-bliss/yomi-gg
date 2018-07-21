7a9feec10c223d1dde51d7bf7e12bf14c652e800
Jack Bliss <jackbliss99@gmail.com>
2018-07-21 12:43:29 +0100

    added basic 'high score' tracking

--- src/endpoints/profile.endpoint.ts
+++ src/endpoints/profile.endpoint.ts
@@ -5,6 +5,7 @@ import { AdminPreprocessor } from '../preprocessors/admin.preprocessor';
 import { MemberPreprocessor } from '../preprocessors/member.preprocessor';
 import { Profile } from '../models/profile.model';
 import * as escape from 'pg-escape';
+import { MatchBetExpanded } from '../models/match-bet-expanded.model';
 
 @Path('/profile')
 export class ProfileEndpoint {
@@ -51,6 +52,48 @@ export class ProfileEndpoint {
     const query = 'SELECT * FROM profiles WHERE id=' + session.profile.id;
     return pool.query(query).then(response => {
       return new Profile(response.rows[0]);
+    });
+  }
+
+  @Path('/highest/total')
+  @GET
+  @Preprocessor(AdminPreprocessor)
+  getHighestCoins(
+    @ContextRequest { pool }: RequestExtended,
+  ): Promise<Profile[]> {
+    const query = 'SELECT * FROM profiles ORDER BY coins DESC';
+    return pool.query(query).then(response => {
+      return response.rows.map(p => new Profile(p));
+    })
+  }
+
+  @Path('/highest/payout')
+  @GET
+  @Preprocessor(AdminPreprocessor)
+  getHighestPayout(
+    @ContextRequest { pool }: RequestExtended,
+  ): Promise<{
+    username: string,
+    bet: MatchBetExpanded,
+  }[]> {
+    const query = 'select ' +
+      'distinct on (profile_id) profile_id, winnings, prediction, wager, outcome, ' +
+      'username, ' +
+      'round, round_order, entrant1id, entrant2id, entrant1tag, entrant2tag, winner, ' +
+      'name as tournament ' +
+      'from match_bets, profiles, matches, events ' +
+      'where ' +
+      'match_bets.profile_id = profiles.id and ' +
+      'match_bets.match_id = matches.id and ' +
+      'matches.event_id = events.id ' +
+      'order by profile_id, winnings desc nulls last';
+    return pool.query(query).then(response => {
+      return response.rows.map(row => {
+        return {
+          username: String(row.username),
+          bet: new MatchBetExpanded(row),
+        };
+      });
     });
   }
 
