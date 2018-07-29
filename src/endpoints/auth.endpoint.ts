2d353bbb8bc40e95cf59b1e494a7d318eb1bd95c
Jack Bliss <jackbliss99@gmail.com>
2018-07-29 13:40:57 +0100

    added some logging to log in endpoint

--- src/endpoints/auth.endpoint.ts
+++ src/endpoints/auth.endpoint.ts
@@ -80,11 +80,13 @@ export class AuthEndpoint {
     return new Promise((resolve, reject) => {
 
       if (!EmailValidator(email)) {
+        console.log('+++++', email);
         setErrorCode(ErrorCodes.INVALID_EMAIL, res);
         throw new Errors.BadRequestError('That email is invalid.');
       }
 
       if (!PasswordValidator(password)) {
+        console.log('+++++', password);
         setErrorCode(ErrorCodes.INVALID_PASSWORD, res);
         throw new Errors.BadRequestError('That password is invalid.');
       }
