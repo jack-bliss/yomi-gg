8d7e267974c4a66fca049e6bb99c38471e8c5d6d
Jack Bliss <jackbliss99@gmail.com>
2018-07-30 12:57:03 +0100

    forgot to add in new subtitle and title files

---
+++ src/client/components/sub-title.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const SubTitle = styled.h3`
+  font-size: 32px;
+  font-weight: normal;
+  margin: 0;
+`;
---
+++ src/client/components/title.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 72px;
+  font-weight: normal;
+  margin: 0;
+  font-style: italic;
+`;
